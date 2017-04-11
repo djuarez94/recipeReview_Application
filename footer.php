1c08d81117f4ab9e4a65d1d76dd5345ed34fa1b3
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-04-11 11:44:47 -0700

    changed copyright on footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 				</article>
 			</main>
 		</div>
-		<footer>Prototype &copy; <?php echo date('Y');?>. All rights reserved.</footer>
+		<footer> &copy; <?php echo date('Y');?> Juarez Design. All rights reserved.</footer>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
